401f88ba1769d7d4442026df4fdfdcdc935b892f
ho9science <full17@naver.com>
2023-07-25 01:15:55 +0900

    feat: history class가 화면에서 가운데 올 때 opacity 1로 변경

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -6,6 +6,37 @@ import { Main } from '@/templates/Main';
 
 import { getStaticPaths, makeStaticProps } from '../../../lib/getStatic';
 
+const History = () => {
+  return (
+    <div>
+      <ul>
+        <li className="history">
+          2023. 05 &lt;포스트모던 어린이 [2부]: 까다로운 어린이를 위해 특별한
+          음식을 준비하지 마세요&gt;, 부산현대미술관
+        </li>
+        <li className="history">2021. 01 &lt;Play on AI &gt;, 아트센터나비</li>
+        <li className="history">
+          2020. 01 &lt;A.I. 632: Design Around Table&gt;, 무등갤러리·토탈미술관
+        </li>
+        <li className="history">
+          2019. 12 &lt;기억장치 - Virtual Memory&gt;, 대안공간루프
+        </li>
+        <li className="history">2019.06 &lt;A.I.632&gt;, 토탈미술관</li>
+        <li className="history">
+          2019. 03 SXSW 2019 COREA IMPACT SHOWCASE 공연, 한국콘텐츠진흥원
+        </li>
+        <li className="history">
+          2018. 10 &lt;A.I.MAGINE Project&gt;, 서울대학교
+          도시데이터사이언스연구소·아트센터나비·스페이스원
+        </li>
+        <li className="history">
+          2017. 11 ICT 문화예술 융합 공모전 Art부문
+          대상(과학기술정보통신부장관상) 수상, 한국정보화진흥원
+        </li>
+      </ul>
+    </div>
+  );
+};
 const Index = () => {
   const router = useRouter();
   const { t } = useTranslation(['common']);
@@ -18,6 +49,7 @@ const Index = () => {
     >
       <img src={`${router.basePath}/assets/images/midiDICE.png`} alt="영상" />
       <h3 className="text-lg font-semibold">{t('history')}</h3>
+      <History />
       <img src={`${router.basePath}/assets/images/midiDICE.png`} alt="이미지" />
       <h3 className="text-lg font-semibold">{t('introduce')}</h3>
       <img src={`${router.basePath}/assets/images/midiDICE.png`} alt="영상2" />
--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useState } from 'react';
 
 import { AppConfig } from '@/utils/AppConfig';
 
@@ -10,64 +10,103 @@ type IMainProps = {
   children: ReactNode;
 };
 
-const Main = (props: IMainProps) => (
-  <div className="w-full px-1 text-gray-700 antialiased">
-    {props.meta}
+const Main = (props: IMainProps) => {
+  const handleScroll = () => {
+    document
+      .querySelector('.history-active')
+      ?.classList.remove('history-active');
+    const element = document.elementFromPoint(
+      window.innerWidth / 2,
+      window.innerHeight / 2
+    );
+    if (element?.classList.contains('history')) {
+      element?.classList.add('history-active');
+    }
+  };
+  const [navbar, setNavbar] = useState<boolean>(false);
+  const handleHamburger = () => {
+    setNavbar(!navbar);
+  };
+  return (
+    <div
+      className="w-full px-1 text-gray-700 antialiased"
+      onWheel={handleScroll}
+    >
+      {props.meta}
+      <div className="mx-auto max-w-screen-md">
+        <header className="border-b border-gray-300">
+          <nav className="flex flex-wrap items-center justify-between bg-black">
+            <div className="mr-6 flex shrink-0 items-center text-white">
+              <span className="text-xl font-semibold tracking-tight">
+                {AppConfig.title}
+              </span>
+            </div>
+            <div
+              className="block lg:hidden"
+              onClick={handleHamburger}
+              role="presentation"
+            >
+              <button
+                className="flex items-center rounded border border-teal-400 px-3 py-2 text-teal-200 hover:border-white hover:text-white"
+                type="button"
+              >
+                <svg
+                  className="h-3 w-3 fill-current"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
+            </div>
+            <div
+              className={`${navbar ? '' : 'hidden'}
+              block w-full grow lg:flex lg:w-auto lg:items-center`}
+            >
+              <div className="text-sm lg:grow">
+                <Link
+                  href="/about/"
+                  className="mr-4 mt-4 block text-teal-200 hover:text-white lg:mt-0 lg:inline-block"
+                >
+                  About
+                </Link>
+                <Link
+                  href="/project/"
+                  className="mr-4 mt-4 block text-teal-200 hover:text-white lg:mt-0 lg:inline-block"
+                >
+                  Proejct
+                </Link>
+              </div>
+              <div className="mt-4 block text-teal-200 lg:mt-0 lg:inline-block">
+                <LanguageSwitchLink
+                  locale="ko"
+                  className="block text-teal-200 lg:mt-0 lg:inline-block"
+                >
+                  KO
+                </LanguageSwitchLink>
+                <span className="block text-teal-200 lg:mt-0 lg:inline-block">
+                  |
+                </span>
+                <LanguageSwitchLink
+                  locale="en"
+                  className="block text-teal-200 lg:mt-0 lg:inline-block"
+                >
+                  EN
+                </LanguageSwitchLink>
+              </div>
+            </div>
+          </nav>
+        </header>
 
-    <div className="mx-auto max-w-screen-md">
-      <header className="border-b border-gray-300">
-        <h1 className="text-3xl font-bold text-gray-900">
-          <Link
-            href="/"
-            className="border-none text-gray-700 hover:text-gray-900"
-          >
-            {AppConfig.title}
-          </Link>
-        </h1>
-        <nav className="grow items-center lg:flex">
-          <ul className="flex list-none flex-col lg:ml-auto lg:flex-row">
-            <li className="ml-20 mr-6">
-              <Link
-                href="/about/"
-                className="border-none text-gray-700 hover:text-gray-900"
-              >
-                About
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link
-                className="border-none text-gray-700 hover:text-gray-900"
-                href="/project/"
-              >
-                Project
-              </Link>
-            </li>
-            <li className="mr-6">
-              <LanguageSwitchLink
-                locale="ko"
-                className="border-none text-gray-700 hover:text-gray-900"
-              >
-                KO
-              </LanguageSwitchLink>
-              |
-              <LanguageSwitchLink
-                locale="en"
-                className="border-none text-gray-700 hover:text-gray-900"
-              >
-                EN
-              </LanguageSwitchLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
+        <main className="content py-5 text-xl">{props.children}</main>
 
-      <main className="content py-5 text-xl">{props.children}</main>
-
-      <footer className="border-t border-gray-300 py-8 text-center text-sm">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}
-      </footer>
+        <footer className="border-t border-gray-300 py-8 text-center text-sm">
+          © Copyright {new Date().getFullYear()} {AppConfig.title}
+        </footer>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { Main };
